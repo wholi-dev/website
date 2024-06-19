b26c62f09be58a58a7e6723784527aa360f91108
Hamnivore <samuelhwhitlock@gmail.com>
2024-06-19 10:16:05 -0600

    more vercel debug

--- src/app/farmer/dashboard/page.tsx
+++ src/app/farmer/dashboard/page.tsx
@@ -1,17 +1,10 @@
 'use client';
-import { useEffect, useState } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
-import { Session } from '@supabase/supabase-js';
 import Link from 'next/link';
 
-export default function signOut() {
-    useEffect(() => {
-        supabase.auth.signOut()
-    }, []);
-    
+export default function dashboard() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-8 bg-[color:var(--color-background)] text-[color:var(--color-text)] font-body">
-        <h1 className="text-4xl font-bold mb-8">Signing out...</h1>
+        <div>
+        <p>this will eventually be the farmer dashboard</p>
         <Link href='/'>Return to homepage</Link>
         </div>
     );
