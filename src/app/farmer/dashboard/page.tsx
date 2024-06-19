ee9cb99ca3f82041a112a5e9708b45f235d5dca3
Hamnivore <samuelhwhitlock@gmail.com>
2024-06-19 10:14:01 -0600

    more vercel debug

--- src/app/farmer/dashboard/page.tsx
+++ src/app/farmer/dashboard/page.tsx
@@ -1,25 +1,18 @@
 'use client';
-
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { useEffect, useState } from 'react';
 import { supabase } from '../../../lib/supabaseClient';
 import { Session } from '@supabase/supabase-js';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function signOut() {
-    const [session, setSession] = useState<Session | null>(null);
-    const router = useRouter();
-    
     useEffect(() => {
-        supabase.auth.signOut().then(() => {
-        router.push('/');
-        });
+        supabase.auth.signOut()
     }, []);
     
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-8 bg-[color:var(--color-background)] text-[color:var(--color-text)] font-body">
         <h1 className="text-4xl font-bold mb-8">Signing out...</h1>
+        <Link href='/'>Return to homepage</Link>
         </div>
     );
     }
