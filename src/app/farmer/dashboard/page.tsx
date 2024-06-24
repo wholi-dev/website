079b49ad25adf93f3d9b419da10323859c18ca13
Hamnivore <samuelhwhitlock@gmail.com>
2024-06-24 12:27:04 -0600

    disabled es-linting rule for no-unescaped-entities

--- src/app/farmer/dashboard/page.tsx
+++ src/app/farmer/dashboard/page.tsx
@@ -2,10 +2,10 @@
 import Link from 'next/link';
 
 export default function dashboard() {
-    return (
-        <div>
-        <p>this will eventually be the farmer dashboard</p>
-        <Link href='/'>Return to homepage</Link>
-        </div>
-    );
-    }
+  return (
+    <div>
+    <p>this will eventually be the farmer dashboard</p>
+    <Link href='/'>Return to homepage</Link>
+    </div>
+  );
+  }
